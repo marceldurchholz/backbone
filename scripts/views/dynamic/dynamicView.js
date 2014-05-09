6b108fff951451fa5467c7c8c3b2448a5647f4b6
DigitalVerve <info@digitalverve.de>
2014-05-09 12:59:17 +0200

    09.05.2014_12:59:16,83

--- scripts/views/dynamic/dynamicView.js
+++ scripts/views/dynamic/dynamicView.js
@@ -1,23 +1,62 @@
-/**
- * Created by Piotr Walczyszyn (outof.me | @pwalczyszyn)
- *
- * User: pwalczys
- * Date: 2/16/12
- * Time: 9:53 AM
- */
+define(['underscore', 'Backbone'],
+    function (_, Backbone) { 
 
-define(['jquery', 'underscore', 'Backbone', 'text!views/dynamic/DynamicView.html'],
-    function ($, _, Backbone, DynamicViewTemplate) {
+        var DynamicViewVar = Backbone.View.extend({
 
-        var DynamicView = Backbone.View.extend({
-
+			events:{
+                'click a':global_a_clickHandler,
+                'click button':global_button_clickHandler,
+            },
+			initialize:function() {
+				_this = this;
+				$(this.el).undelegate('a', 'click');
+			},
+			putTemplateContent: function() {
+				var output = _.template(_this.options.dynContent,{
+					page_vars: _this.options
+				}, {variable:'page_vars'});
+				$(this.el).html(output);
+				
+			},
             render:function () {
-                this.$el.html(_.template(DynamicViewTemplate));
-                return this;
-            }
+				var page_vars = _this.options;
+				_this.options.dynContent = _this.options.model.get('dynContent');
+				_this.options.templateUrl = _this.options.model.get('templateUrl');
+				
+				if (_this.options.dynContent && _this.options.dynContent!='') contentExists = true;
+				else contentExists = false;
+				if (_this.options.templateUrl && _this.options.templateUrl!='') fileExists = true;
+				else fileExists = false;
+				if (contentExists==false && fileExists==false) {
+					_this.options.templateUrl = 'text!views/dynamic/DynamicView.html';
+					fileExists=true;
+				}
+				
+				if (fileExists==false) {
+					var output = _.template(_this.options.dynContent,{
+						page_vars: _this.options
+					}, {variable:'page_vars'});
+					$(this.el).html(output);
+				}
+				else {
+					require([_this.options.templateUrl], function(wrapperContent) {
+						var _wrapperContent = wrapperContent;
+						var dynContent = _.template(_wrapperContent,{
+							page_vars: _this.options,
+							myvar: 'foo'
+						});
+						var finalContent = _.template(dynContent,{
+							page_vars: _this.options,
+							myvar: 'foo'
+						});
+						$(_this.el).html(finalContent);
+					});
+				}
 
+				return _this;
+            }
 
         });
 
-        return DynamicView;
+        return DynamicViewVar;
     });
